5e00266160f1b2992da16a490e63d1d416a97544
Oran Fry <oran@emerg.co>
2020-07-08 06:50:45 +1200

    Token passing

--- src/php/controller/import.php
+++ src/php/controller/import.php
@@ -8,7 +8,7 @@ while ($f = fgets(STDIN)) {
 
     $print = "{$linetype_name} ";
     echo str_pad($print, 30, '.');
-    $data = $linetype->save(json_decode($rawdata), 0, $timestamp);
+    $data = $linetype->save(AUTH_TOKEN, json_decode($rawdata), 0, $timestamp);
 
     echo str_pad(' ' . count($data), 10, '.', STR_PAD_LEFT) . "\n";
 }
